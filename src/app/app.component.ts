e53d337a5ddab97340bc12f7c57b5301076330f8
Matthias Michel <mam@nexplore.ch>
2024-09-22 17:29:45 +0200

    Moved Angular Project for deployment

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from './common/footer/footer.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, FooterComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+})
+export class AppComponent {}
