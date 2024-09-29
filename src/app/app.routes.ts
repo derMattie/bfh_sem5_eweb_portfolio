bf2e016b835f50b2a836e635fd1fb04ace4e676e
derMatt <matthiasmichel@me.com>
2024-09-29 16:42:44 +0200

    Added 404-Page and Portfolio Component to start

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,15 @@
 import { Routes } from '@angular/router';
+import { Error404Component } from './error404/error404.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
 
 export const routes: Routes = [
+  // Routes
+  { path: 'portfolio', component: PortfolioComponent },
+
   // Errors
-  //   { path: 'error404', component: ErrorPagenotfoundComponent },
+  { path: 'error404', component: Error404Component },
 
   // Common Routes
-  { path: '', redirectTo: '/tracks', pathMatch: 'full' }, // Redirects to /tracks as the default route
+  { path: '', redirectTo: '/portfolio', pathMatch: 'full' }, // Redirects to /tracks as the default route
   { path: '**', redirectTo: '/error404', pathMatch: 'full' }, // Wildcard route for a 404 page, redirected to /artists
 ];
---
+++ src/app/error404/error404.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-error404',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './error404.component.html',
+  styleUrl: './error404.component.scss',
+})
+export class Error404Component {}
